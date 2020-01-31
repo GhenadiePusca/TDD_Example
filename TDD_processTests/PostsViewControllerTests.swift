df99f013e5d204b9c7d9a016470734c7b6fb5419
Ghenadie Pusca <pusca.ghenadie@gmail.com>
2020-01-31 15:06:41 +0200

    Initial commit
    Exemplified the use cases, made initial setup

---
+++ TDD_processTests/PostsViewControllerTests.swift
@@ -0,0 +1,33 @@
+//
+//  PostsViewControllerTests.swift
+//  TDD_processTests
+//
+//  Created by Pusca, Ghenadie on 1/31/20.
+//  Copyright © 2020 Pusca, Ghenadie. All rights reserved.
+//
+
+import Foundation
+import XCTest
+
+/*
+    As a user I do want to see a list of the available posts.
+ 
+    Posts {
+      image
+      description
+    }
+
+    Posts Use cases:
+    - Posts are loaded as soon the screen is visible.
+    - While posts are loading a loading indicator is shown.
+    - If load failed, a error banner is shownß
+    - User can manually reload the posts list.
+ 
+   Post image use case:
+    - Image is loaded once post visible.
+    - While image is loading - a loading indicator is shown.
+    - If image load failed, a retry button is shown.
+    - On retry the image is loaded again.
+ */
+
+class PostsViewControllerTests: XCTestCase { }
