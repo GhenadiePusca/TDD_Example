b69e12f51c3a0a71ec482ba0a2499886d2f64056
Ghenadie Pusca <pusca.ghenadie@gmail.com>
2020-01-31 15:02:53 +0200

    Initial Commit

---
+++ TDD_process/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TDD_process
+//
+//  Created by Pusca, Ghenadie on 1/31/20.
+//  Copyright © 2020 Pusca, Ghenadie. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
